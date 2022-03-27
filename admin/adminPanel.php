7046cba10b81aca82237d6f83ccc35aeb033ed47
INJOKER11 <mig20047@gmail.com>
2022-03-28 00:12:39 +0200

    products in adminPanel

--- admin/adminPanel.php
+++ admin/adminPanel.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+include 'C:\OpenServer\domains\auth\vendor\connect.php';
+include 'C:\OpenServer\domains\auth\functions.php';
+
+$result = mysqli_query($connect, "SELECT * FROM `goods`");
 ?>
 
 <!doctype html>
@@ -22,11 +26,6 @@ session_start();
             <input type="checkbox" class="product_check" >
             <label for="product_check">Products</label>
 
-
-
-
-
-
         </div>
         <div class="second_item">
 
@@ -36,28 +35,64 @@ session_start();
         </div>
     </div>
 
+    <!--
+        <div class="admin_form_wrapper">
+            <button type="submit" class="admin_add_product">
+                Добавить продукт
+            </button>
 
-    <div class="admin_form_wrapper">
-        <button type="submit" class="admin_add_product">
-            Добавить продукт
-        </button>
+            <form action="controller/productController.php" method="post" enctype="multipart/form-data"  class="admin_form" >
+                <label >Фото продукта</label>
+                <input type="file" name="product_img">
+                <label >Название продукта</label>
+                <input type="text" name="product_name" placeholder="Введи название продукта">
+                <label >Описание продукта</label>
+                <textarea type="text" name="product_description" placeholder="Введи описание продукта" ></textarea>
+                <label >Стоимость продукта</label>
+                <input type="number" name="product_price" placeholder="Введи стоимость продукта">
+                <label>Веддите валюту</label>
+                <input  type="text" name="currency" placeholder="Введите валюту">
+                <button type="submit">Добавить продукт</button>
+            </form>
 
-        <form action="controller/productController.php" method="post" enctype="multipart/form-data"  class="admin_form" >
-            <label >Фото продукта</label>
-            <input type="file" name="product_img">
-            <label >Название продукта</label>
-            <input type="text" name="product_name" placeholder="Введи название продукта">
-            <label >Описание продукта</label>
-            <textarea type="text" name="product_description" placeholder="Введи описание продукта" ></textarea>
-            <label >Стоимость продукта</label>
-            <input type="number" name="product_price" placeholder="Введи стоимость продукта">
-            <label>Веддите валюту</label>
-            <input  type="text" name="currency" placeholder="Введите валюту">
-            <button type="submit">Добавить продукт</button>
-        </form>
+        </div>
+    -->
 
-    </div>
 
+
+    <div class="product_in_admin_wrapper">
+
+
+
+    <table>
+
+        <tr>
+            <th>id</th>
+            <th>Изображение</th>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Цена</th>
+            <th>Валюта</th>
+            <th>Редактировать</th>
+        </tr>
+        <?php  foreach($result as $admin_products){     ?>
+        <tr>
+            <td><?=  $admin_products['id']  ?></td>
+            <td>
+                <img src="../<?=  $admin_products['product_img']  ?>" alt="" style="height: 100px" class="img_in_admin">
+            </td>
+            <td><?=  $admin_products['product_name']  ?></td>
+            <td><?=  $admin_products['product_description']  ?></td>
+            <td><?=  $admin_products['product_cost']  ?></td>
+            <td><?=  $admin_products['currency']  ?></td>
+            <td><a href="" class="">Редактировать</a></td>
+
+        </tr>
+
+        <?php     } ?>
+    </table>
+
+</div>
 </div>
 
 
--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,16 @@
 var checkBox = document.querySelector('.product_check');
 var adminForm = document.querySelector('.admin_form_wrapper');
-adminForm .style.display = 'none';
+adminForm.style.display = 'none';
 
 checkBox.addEventListener('change', () => {
 
     if(checkBox.checked) {
 
-        let prod_dir = '/admin/adminProducts.php';
+            let prod_dir = '/admin/adminProducts.php';
 
-        if(window.location.pathname === prod_dir){
-            adminForm .style.display = 'flex';
-        }else{
+        if (window.location.pathname === prod_dir) {
+            adminForm.style.display = 'flex';
+        } else {
 
             window.location.replace('/admin/adminProducts.php');
         }
